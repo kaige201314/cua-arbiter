aa7b010f7f8d47273e8a4f3f76adce7011c57508
wohui <wohui@xxx.com>
2017-09-11 17:49:25 +0800

    1.修改logstash——>redis  key值 2.修改前端查询日志页显示 3.修改setting的USE_TZ = True

--- arbiter-web/arbiter/static/arbiter/js/wholog/home.js
+++ arbiter-web/arbiter/static/arbiter/js/wholog/home.js
@@ -76,9 +76,29 @@ $(document).ready(function() {
         loadData();
 
     });
-
+    /*init 日志详情模态*/
+    $('#log_detail_modal').modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .2, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      starting_top: '40%', // Starting top style attribute
+      ending_top: '10%', // Ending top style attribute
+      ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+        alert("Ready");
+        console.log(modal, trigger);
+      },
+      complete: function() {
+          alert('Closed');
+      } // Callback for Modal close
+    }
+  );
 
 });/*ready end*/
+
+
+
+
 function getCurrentTime() {
      var current = new Array();
      var currentTime = new Date();
@@ -166,9 +186,7 @@ function loadData() {
                 "render": function(data, type, row, meta) {
                     //替换所有-
                     logIdData = data.replace(/\-/g,"");
-
-                     // return data = '<button class="btn btn-info btn-sm" data-id=' + data + '><i class="fa fa-pencil"></i>Edit</button>';
-                     return data ='<a href="../wholog/logDetail?logId='+logIdData+'" class="waves-effect waves-light btn">查看</a>'
+                     return  '<a type="button"  onclick=openLogDetail(logIdData) class="waves-effect waves-light btn">查看</a>';
                 }
             }],
         //使用ajax请求
@@ -204,6 +222,34 @@ function loadData() {
     } );
 
 }/*load Data end*/
+
+/*打开日志详情模态框*/
+function openLogDetail(logId){
+     $('#log_detail_modal').modal('open');
+     /*打印日志内容*/
+    /*ajax请求*/
+      $.ajax({
+          type:'GET',
+          url:'../wholog/queryLogData',
+          data:{'logId':logId},
+          dataType:'json',
+          success:function (data,status) {
+              /*循环输出*/
+              $.each(data.data,function (i,val) {
+
+                   document.getElementById("log_data").innerHTML += "<li><a>" + val + "</a></li>";
+              });
+
+          },
+          error:function (err,status) {
+              alert("出现了一个错误:"+status);
+          }
+      });
+
+
+}
+
+
 
 
 
